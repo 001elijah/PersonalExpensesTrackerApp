67aa2c6f34b50f9e02d416806bec34809818d195
illia zolotukha <job.illia.igorevich@gmail.com>
2025-05-05 22:23:39 +0300

    feat: add updateExpense functionality across app feat: implemented updateExpense logic in Redux slice, operations, and Firebase API feat: modified `ManageExpenseScreen` to handle expense updates, ensuring proper dispatch for updated data feat: adjusted formatting and removed console logs for cleaner codebase

--- screens/ManageExpenseScreen.tsx
+++ screens/ManageExpenseScreen.tsx
@@ -4,51 +4,60 @@ import {useDispatch} from 'react-redux';
 
 import {ExpenseForm} from '../components/ExpenseForm.tsx';
 import {useDatePicker} from '../hooks/useDatePicker.ts';
-import {deleteExpense} from '../redux/operations/expensesOperations.ts';
+import {
+  deleteExpense,
+  updateExpense,
+} from '../redux/operations/expensesOperations.ts';
 import {AppDispatch} from '../redux/store.ts';
 import {Expense, ExpenseData} from '../types/ExpenseTypes.ts';
 import {HomeStackParamList} from '../types/HomeStackParamList.ts';
 
 export const ManageExpenseScreen = () => {
-  const route = useRoute<RouteProp<HomeStackParamList, 'ManageExpenseScreen'>>();
-  const { expense } = route.params;
+  const route =
+    useRoute<RouteProp<HomeStackParamList, 'ManageExpenseScreen'>>();
+  const {expense} = route.params;
   const dispatch = useDispatch<AppDispatch>();
-  const [expenseData, setExpenseData] = useState<ExpenseData>({...expense});
+  const [expenseFormData, setExpenseFormData] = useState<ExpenseData>({
+    ...expense,
+  });
   const {showDatePicker, setShowDatePicker, handleDatePress} = useDatePicker({
-    date: new Date(expenseData.date as string),
-    onDateChange: (selectedDate) => setExpenseData({...expenseData, date: selectedDate}),
+    date: new Date(expenseFormData.date as string),
+    onDateChange: selectedDate =>
+      setExpenseFormData({...expenseFormData, date: selectedDate}),
   });
   const handleSubmit = () => {
-      console.log('Saving expense: ', {
-        ...expenseData,
-        amount: Number(expenseData.amount),
-      });
-      // TODO: Dispatch action to save expense
+    const expenseToSubmit = {
+      ...expenseFormData,
+      amount: parseFloat(
+        expenseFormData.amount?.toString().replace(',', '.') || '0',
+      ),
+    };
+    dispatch(updateExpense(expenseToSubmit));
   };
 
   const handleDeleteExpense = (id: ExpenseData['id']) => {
-    console.log('Deleting expense by id: ', id);
-    // TODO: Dispatch action to delete expense
     if (id) {
       dispatch(deleteExpense(id));
     }
   };
 
-  const handleExpenseChange = (expenseDataToHandle: Expense | ExpenseData) => {
-    setExpenseData(expenseDataToHandle as ExpenseData);
+  const handleExpenseChange = (
+    expenseFormDataToHandle: Expense | ExpenseData,
+  ) => {
+    setExpenseFormData(expenseFormDataToHandle as ExpenseData);
   };
 
   return (
     <ExpenseForm
       title={'Manage Expense'}
       submitTitle={'Update Expense'}
-      expense={expenseData}
+      expense={expenseFormData}
       onSetExpense={handleExpenseChange}
       onDatePress={handleDatePress}
       onSubmit={handleSubmit}
       showDatePicker={showDatePicker}
       onShowDatePicker={setShowDatePicker}
-      onDelete={() => handleDeleteExpense(expenseData.id)}
+      onDelete={() => handleDeleteExpense(expenseFormData.id)}
     />
   );
 };
--- services/firebaseAPI.ts
+++ services/firebaseAPI.ts
@@ -1,9 +1,10 @@
-import {FirebaseAuthTypes,getAuth} from '@react-native-firebase/auth';
+import {FirebaseAuthTypes, getAuth} from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
 import {FirestoreExpenseData} from '../types/ExpenseTypes.ts';
 import {User, LoginData, RegisterData} from '../types/User.ts';
 import {transformExpenseData} from '../utils/transformExpensesData.ts';
+
 const auth = getAuth();
 
 export const registerAPI = async ({
@@ -32,26 +33,55 @@ export const authStateChanged = (
 };
 
 export const getUserProfileAPI = () => {
-  return new Promise<FirebaseAuthTypes.User | null>((resolve) => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
+  return new Promise<FirebaseAuthTypes.User | null>(resolve => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
       unsubscribe(); // Unsubscribe immediately after getting the user
       resolve(user);
     });
   });
 };
 
-export const createExpenseAPI = async ({uid, title, amount, category, date}: FirestoreExpenseData): Promise<FirestoreExpenseData> => {
-  const docRef = await firestore().collection('expenses').add({uid, title, amount, category, date});
+export const createExpenseAPI = async ({
+  uid,
+  title,
+  amount,
+  category,
+  date,
+}: FirestoreExpenseData): Promise<FirestoreExpenseData> => {
+  const docRef = await firestore()
+    .collection('expenses')
+    .add({uid, title, amount, category, date});
   const docSnapshot = await docRef.get();
   const data = docSnapshot.data();
   return transformExpenseData(docSnapshot.id, data);
 };
 
-export const readExpensesAPI = async (uid: User['uid']): Promise<FirestoreExpenseData[]> => {
-  const snapshot = await firestore().collection('expenses').where('uid', '==', uid).get();
-  return snapshot.docs.map((doc) =>
-    transformExpenseData(doc.id, doc.data()),
-  );
+export const readExpensesAPI = async (
+  uid: User['uid'],
+): Promise<FirestoreExpenseData[]> => {
+  const snapshot = await firestore()
+    .collection('expenses')
+    .where('uid', '==', uid)
+    .get();
+  return snapshot.docs.map(doc => transformExpenseData(doc.id, doc.data()));
+};
+
+export const updateExpenseAPI = async ({
+  id,
+  title,
+  amount,
+  category,
+  date,
+}: FirestoreExpenseData) => {
+  if (id) {
+    await firestore()
+      .collection('expenses')
+      .doc(id)
+      .update({title, amount, category, date});
+    const docSnapshot = await firestore().collection('expenses').doc(id).get();
+    const updatedData = docSnapshot.data();
+    return transformExpenseData(docSnapshot.id, updatedData);
+  }
 };
 
 export const deleteExpenseAPI = async (id: string) => {
